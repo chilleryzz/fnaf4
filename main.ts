208bf50ebc7c933fb5ba5c8d1f1b4329cfa5df54
chilleryzz <71833391+chilleryzz@users.noreply.github.com>
2022-01-19 12:41:17 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,29 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         roomCurrent.destroy()
         roomDirection4 = 15
         roomRight = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
+        mySprite = 0
     } else if (roomDirection == 15) {
         roomCurrent.destroy()
         roomDirection5 = 10
         room3 = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
+        mySprite = 0
     } else {
     	
+    }
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (roomDirection == 5) {
+    	
+    } else if (roomDirection == 10) {
+    	
+    } else if (roomDirection == 15) {
+    	
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite == 50) {
+        roomBack.destroy()
+        roomBackLight = sprites.create(assets.image`bedRoomOn0`, SpriteKind.Player)
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -17,10 +34,12 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         roomCurrent.destroy()
         roomDirection2 = 5
         roomCurrent2 = sprites.create(assets.image`leftRoom`, SpriteKind.Projectile)
+        mySprite = 0
     } else if (roomDirection == 15) {
         roomCurrent.destroy()
         roomDirection3 = 10
         room2 = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
+        mySprite = 0
     } else {
     	
     }
@@ -30,10 +49,52 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         roomCurrent.destroy()
         roomDirection4 = 15
         roomRight = sprites.create(assets.image`rightRoom`, SpriteKind.Projectile)
+        mySprite = 0
     } else if (roomDirection == 15) {
         roomCurrent.destroy()
         roomDirection5 = 10
         room3 = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
+        mySprite = 0
+    } else {
+    	
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    if (mySprite == 50) {
+        roomBackLight.destroy()
+        roomBack = sprites.create(assets.image`bedRoomOff`, SpriteKind.Player)
+    }
+})
+function test () {
+    controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+        test()
+        
+    })
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (roomDirection == 10) {
+        room.destroy()
+        roomCurrent.destroy()
+        roomDirection2 = 20
+        animation.runImageAnimation(
+        backAnim,
+        assets.animation`bedAnim`,
+        50,
+        false
+        )
+        roomBack = sprites.create(assets.image`bedRoomOff`, SpriteKind.Projectile)
+        mySprite = 50
+    } else if (roomDirection == 15) {
+        roomCurrent.destroy()
+        roomDirection3 = 20
+        animation.runImageAnimation(
+        backAnim,
+        assets.animation`bedAnim`,
+        50,
+        false
+        )
+        roomBack = sprites.create(assets.image`bedRoomOff`, SpriteKind.Projectile)
+        mySprite = 50
     } else {
     	
     }
@@ -42,13 +103,18 @@ let room2: Sprite = null
 let roomDirection3 = 0
 let roomCurrent2: Sprite = null
 let roomDirection2 = 0
+let roomBackLight: Sprite = null
+let roomBack: Sprite = null
 let room3: Sprite = null
 let roomDirection5 = 0
+let mySprite = 0
 let roomRight: Sprite = null
 let roomDirection4 = 0
 let roomDirection = 0
 let roomCurrent: Sprite = null
 let room: Sprite = null
+let backAnim: Sprite = null
+backAnim = sprites.create(assets.image`bedRoomOff`, SpriteKind.Projectile)
 room = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
 roomCurrent = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
 roomDirection = 10
