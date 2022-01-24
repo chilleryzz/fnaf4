c2d0c248ccea77b70d0b2e09cf59d3bcc921ab3b
chilleryzz <71833391+chilleryzz@users.noreply.github.com>
2022-01-24 12:36:07 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,12 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         facing = 10
     }
 })
+function freddlesGo1 () {
+    room.setImage(assets.image`bedRoomOnFred1`)
+    pause(200)
+    freddles = 0
+    room.setImage(assets.image`bedRoomOn0`)
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (facing == 5) {
         room.setImage(assets.image`leftDoor`)
@@ -33,7 +39,15 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (facing == 20) {
-        room.setImage(assets.image`bedRoomOn0`)
+        if (freddles == 3) {
+            freddlesGo3()
+        } else if (freddles == 2) {
+            freddlesGo2()
+        } else if (freddles == 1) {
+            freddlesGo1()
+        } else {
+            room.setImage(assets.image`bedRoomOn0`)
+        }
     } else if (facing == 25) {
         room.setImage(assets.image`leftDoorLight`)
     } else if (facing == 30) {
@@ -42,12 +56,33 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         room.setImage(assets.image`closetLight`)
     }
 })
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    if (facing == 35) {
+        room.setImage(assets.image`closet`)
+        closClosed = 5
+    } else if (facing == 30) {
+        room.setImage(assets.image`rightDoor`)
+        righClosed = 5
+    } else if (facing == 25) {
+        room.setImage(assets.image`leftDoor`)
+        leftClosed = 5
+    }
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (door == 0) {
         room.setImage(assets.image`leftRoom`)
         facing = 5
     }
 })
+function freddlesGo2 () {
+    room.setImage(assets.image`bedRoomOnFred2`)
+    pause(200)
+    room.setImage(assets.image`bedRoomOnFred1`)
+    freddles = 1
+    pause(200)
+    freddles = 0
+    room.setImage(assets.image`bedRoomOn0`)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (door == 0) {
         room.setImage(assets.image`rightRoom`)
@@ -65,15 +100,48 @@ controller.A.onEvent(ControllerButtonEvent.Released, function () {
         room.setImage(assets.image`closet`)
     }
 })
+function freddlesGo3 () {
+    room.setImage(assets.image`bedRoomOnFred3`)
+    pause(200)
+    room.setImage(assets.image`bedRoomOnFred2`)
+    freddles = 2
+    pause(200)
+    room.setImage(assets.image`bedRoomOnFred1`)
+    freddles = 1
+    pause(200)
+    freddles = 0
+    room.setImage(assets.image`bedRoomOn0`)
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (door == 0) {
         room.setImage(assets.image`bedRoomOff`)
         facing = 20
+    } else if (facing == 35) {
+        room.setImage(assets.image`closetClosed`)
+        closClosed = 10
+    } else if (facing == 30) {
+        room.setImage(assets.image`rightDoorClosed`)
+        righClosed = 10
+    } else if (facing == 25) {
+        room.setImage(assets.image`leftDoorClosed`)
+        leftClosed = 10
     }
 })
+let leftClosed = 0
+let righClosed = 0
+let closClosed = 0
+let freddles = 0
 let facing = 0
 let room: Sprite = null
 let door = 0
 door = 0
 room = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
 facing = 10
+forever(function () {
+    pause(20000)
+    freddles += 1
+    pause(5000)
+    freddles += 1
+    pause(2000)
+    freddles += 1
+})
