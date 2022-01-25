53b2557c6e143c554fa719985de07f5cbaf16927
chilleryzz <71833391+chilleryzz@users.noreply.github.com>
2022-01-25 11:38:45 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -38,56 +38,63 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (facing == 20) {
-        if (freddles == 3) {
-            freddlesGo3()
-        } else if (freddles == 2) {
-            freddlesGo2()
-        } else if (freddles == 1) {
-            freddlesGo1()
-        } else {
-            room.setImage(assets.image`bedRoomOn0`)
+    if (closClosed == 0) {
+        if (leftClosed == 0) {
+            if (righClosed == 0) {
+                if (facing == 20) {
+                    if (freddles == 3) {
+                        freddlesGo3()
+                    } else if (freddles == 2) {
+                        freddlesGo2()
+                    } else if (freddles == 1) {
+                        freddlesGo1()
+                    } else {
+                        room.setImage(assets.image`bedRoomOn0`)
+                    }
+                } else if (facing == 25) {
+                    // jumpscare function soon
+                    if (boni == 5) {
+                        room.setImage(assets.image`leftDoorLightBonnieHiding`)
+                        pause(100)
+                        room.setImage(assets.image`rightDoorLightBonnieHiding2`)
+                        pause(100)
+                        room.setImage(assets.image`rightDoorLightBonnieHiding3`)
+                        pause(100)
+                        boni = 0
+                        room.setImage(assets.image`leftDoorLight`)
+                    } else if (boni == 10) {
+                        room.setImage(assets.image`leftDoorLightBonnieHiding`)
+                        pause(100)
+                        room.setImage(assets.image`rightDoorLightBonnieHiding2`)
+                        pause(100)
+                        room.setImage(assets.image`rightDoorLightBonnieHiding3`)
+                        pause(100)
+                        boni = 0
+                        room.setImage(assets.image`leftDoorLight`)
+                    } else if (boni == 15) {
+                    	
+                    } else {
+                        room.setImage(assets.image`leftDoorLight`)
+                    }
+                } else if (facing == 30) {
+                    room.setImage(assets.image`rightDoorLight`)
+                } else if (facing == 35) {
+                    room.setImage(assets.image`closetLight`)
+                }
+            }
         }
-    } else if (facing == 25) {
-        // jumpscare function soon
-        if (boni == 5) {
-            room.setImage(assets.image`leftDoorLightBonnieHiding`)
-            pause(100)
-            room.setImage(assets.image`rightDoorLightBonnieHiding2`)
-            pause(100)
-            room.setImage(assets.image`rightDoorLightBonnieHiding3`)
-            pause(100)
-            room.setImage(assets.image`leftDoorLight`)
-            boni = 0
-        } else if (boni == 10) {
-            room.setImage(assets.image`leftDoorLightBonnieHiding`)
-            pause(100)
-            room.setImage(assets.image`rightDoorLightBonnieHiding2`)
-            pause(100)
-            room.setImage(assets.image`rightDoorLightBonnieHiding3`)
-            pause(100)
-            room.setImage(assets.image`leftDoorLight`)
-        } else if (boni == 15) {
-        	
-        } else {
-            room.setImage(assets.image`leftDoorLight`)
-        }
-    } else if (facing == 30) {
-        room.setImage(assets.image`rightDoorLight`)
-    } else if (facing == 35) {
-        room.setImage(assets.image`closetLight`)
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     if (facing == 35) {
         room.setImage(assets.image`closet`)
-        closClosed = 5
+        closClosed = 0
     } else if (facing == 30) {
         room.setImage(assets.image`rightDoor`)
-        righClosed = 5
+        righClosed = 0
     } else if (facing == 25) {
         room.setImage(assets.image`leftDoor`)
-        leftClosed = 5
+        leftClosed = 0
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -112,14 +119,20 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    if (facing == 20) {
-        room.setImage(assets.image`bedRoomOff`)
-    } else if (facing == 25) {
-        room.setImage(assets.image`leftDoor`)
-    } else if (facing == 30) {
-        room.setImage(assets.image`rightDoor`)
-    } else if (facing == 35) {
-        room.setImage(assets.image`closet`)
+    if (closClosed == 0) {
+        if (leftClosed == 0) {
+            if (righClosed == 0) {
+                if (facing == 20) {
+                    room.setImage(assets.image`bedRoomOff`)
+                } else if (facing == 25) {
+                    room.setImage(assets.image`leftDoor`)
+                } else if (facing == 30) {
+                    room.setImage(assets.image`rightDoor`)
+                } else if (facing == 35) {
+                    room.setImage(assets.image`closet`)
+                }
+            }
+        }
     }
 })
 function freddlesGo3 () {
@@ -149,8 +162,8 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         leftClosed = 10
     }
 })
-let leftClosed = 0
 let righClosed = 0
+let leftClosed = 0
 let closClosed = 0
 let freddles = 0
 let facing = 0
@@ -162,17 +175,17 @@ door = 0
 room = sprites.create(assets.image`frontRoom`, SpriteKind.Projectile)
 facing = 10
 forever(function () {
-    pause(randint(20000, 100000))
+    pause(randint(0, 100000))
     boni = 5
     if (freddles == 0) {
         freddles += 1
     }
-    pause(15000)
+    pause(randint(20000, 100000))
     boni = 10
     if (freddles == 1) {
         freddles += 1
     }
-    pause(5000)
+    pause(randint(20000, 100000))
     boni = 15
     if (freddles == 2) {
         freddles += 1
